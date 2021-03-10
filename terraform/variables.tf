dc2dfd2bde96420accbdcd2266da0fc1bef0ce37
cachs1 <cachs1@hotmail.com>
2021-03-10 01:58:53 -0700

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "profile" {
+  type    = string
+  default = "default"
+}
+
+variable "region-master" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "s3-name" {
+  type    = string
+  default = "bucket-prueba-blueoptima"
+}
