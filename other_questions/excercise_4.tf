cf33fe6723ccda45543fb4e89da73e588f1cd34c
cachs1 <cachs1@hotmail.com>
2021-03-10 08:08:31 -0700

    Create excercise_4.tf

---
+++ other_questions/excercise_4.tf
@@ -0,0 +1,233 @@
+# profile 
+variable "profile" {
+  type    = "string"
+  default = "default"
+}
+
+# aws
+provisioner "aws" {
+  profile = var.profile
+  region  = var.region-master
+  alias   = "region-master"
+}
+
+#vpc
+resource "aws_vpc" "vpc_master" {
+  provider             = aws.region-master
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "master-vpc"
+  }
+}
+
+#Create IGW in us-east-1
+resource "aws_internet_gateway" "igw" {
+  provider = aws.region-master
+  vpc_id   = aws_vpc.vpc_master.id
+}
+
+#Get all available AZ's in VPC for master region
+data "aws_availability_zones" "azs" {
+  provider = aws.region-master
+  state    = "available"
+}
+
+
+#Create subnet # 1 in us-east-1
+resource "aws_subnet" "subnet_1" {
+  provider          = aws.region-master
+  availability_zone = element(data.aws_availability_zones.azs.names, 0)
+  vpc_id            = aws_vpc.vpc_master.id
+  cidr_block        = "10.0.1.0/24"
+}
+
+#Create subnet #2  in us-east-1
+resource "aws_subnet" "subnet_2" {
+  provider          = aws.region-master
+  vpc_id            = aws_vpc.vpc_master.id
+  availability_zone = element(data.aws_availability_zones.azs.names, 1)
+  cidr_block        = "10.0.2.0/24"
+}
+
+#Create route table in us-east-1
+resource "aws_route_table" "internet_route" {
+  provider = aws.region-master
+  vpc_id   = aws_vpc.vpc_master.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+  tags = {
+    Name = "Master-Region-RT"
+  }
+}
+
+#Overwrite default route table of VPC with our route table 
+resource "aws_main_route_table_association" "set-master-default-rt-assoc" {
+  provider       = aws.region-master
+  vpc_id         = aws_vpc.vpc_master.id
+  route_table_id = aws_route_table.internet_route.id
+}
+
+#Create acl on subnet-1
+resource "aws_network_acl" "acl_subnet_1" {
+  vpc_id     = aws_vpc.vpc_master.id
+  subnet_ids = aws_subnet.subnet_1.id
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "10.0.1.0/16"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "10.0.1.0/16"
+    from_port  = 22
+    to_port    = 22
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "10.0.2.0/16"
+    from_port  = 22
+    to_port    = 22
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "10.0.2.0/16"
+    from_port  = 3306
+    to_port    = 3306
+  }
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 300
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  tags = {
+    Name = "Master-ACL-Subnet1"
+  }
+}
+
+#Security group 1
+resource "aws_security_group" "sg_1" {
+  provider = aws.region-master
+  name     = "sg_1"
+  vpc_id   = aws_vpc.vpc_master.id
+
+  ingress {
+    description = "Allow connection from 80"
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["10.0.1.0/16"]
+  }
+
+  ingress {
+    description = "Allow connection from 22"
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["10.0.1.0/16"]
+  }
+
+  egress {
+    description = "Allow outgoing from 80"
+    protocol    = "tcp"
+    from_port   = 3306
+    to_port     = 3306
+    cidr_blocks = ["10.0.2.0/16"]
+  }
+
+  egress {
+    description = "Allow outgoing from 80"
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["10.0.1.0/16"]
+  }
+
+  egress {
+    description = "Allow outgoing from 443"
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["10.0.1.0/16"]
+  }
+
+  egress {
+    description = "Allow ssh from host"
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = [var.external_ip]
+  }
+}
+
+#security group 2
+resource "aws_security_group" "sg_2" {
+  provider = aws.region-master
+  name     = "sg_2"
+  vpc_id   = aws_vpc.vpc_master.id
+
+  ingress {
+    description = "Allow connection from 22"
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["10.0.1.0/16"]
+  }
+
+  ingress {
+    description     = "Allow connection from subnet_2"
+    protocol        = "tcp"
+    from_port       = 3306
+    to_port         = 3306
+    security_groups = [aws_security_group.sg_1.id]
+  }
+
+  egress {
+    description = "Allow outgoing from 80"
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["10.0.1.0/16"]
+  }
+
+  egress {
+    description = "Allow outgoing from 443"
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["10.0.1.0/16"]
+  }
+}
